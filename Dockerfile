0d1a4cfbab573fe711065ca3480d544aec24612d
Alexander Kampf <akampftcg@gmail.com>
2020-08-27 08:41:11 +0200

    Fix production setup

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM nginx:stable
 
 WORKDIR /app
 RUN apt-get update
+RUN apt-get install -y npm
+RUN npm install --global uglify-js
 RUN curl -L -o elm.gz https://github.com/elm/compiler/releases/download/0.19.1/binary-for-linux-64-bit.gz
 RUN gunzip elm.gz
 RUN chmod +x elm
--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 if [ "${1:-"--dev"}" = "--prod" ]
 then
 
-elm make src.elm --optimize --output dist/unminified_main.js &&
-uglifyjs dist/unminified_main.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=dist/main.js
+elm make src/Main.elm --optimize --output dist/unminified_main.js &&
+uglifyjs dist/unminified_main.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output dist/main.js
 
 else
 
